eaa0a8886a4f675661850dabb7731628e151325f
luba-kn <luba_kn@mail.ru>
2018-05-30 19:40:48 +0300

    ver2

--- task1.rb
+++ task1.rb
@@ -8,54 +8,23 @@
 
 # encoding: utf-8
 
+if (ARGV.empty? )
+	puts "Мало данных"
+		exit
+	elsif ARGV.length >= 2
+	puts "Много данных"
+	exit
+end
 
-delChar1 = "НнРрКк"
-delChar = ["Н","н","Р","р","К","к"]
 str = ARGV[0]
-newStr = []
 n = str.length
-
 if (n<1 || n>10000)
 	puts "не та длина строки"
 	exit
 end
-puts delChar
 
-for i in 0..n
-	puts str[i]
-#	
-#
-#	if !(sym.object_id != "P".to_sym.object_id || sym.object_id != "р".to_sym.object_id || sym.object_id != "К".to_sym.object_id || sym.object_id != "к".to_sym.object_id || sym.object_id != "Н".to_sym.object_id || sym.object_id != "н".to_sym.object_id )
-	  
+str = str.encode('utf-8')
 
-#	  newStr.push(str[i])
-#  end
-	
-#	for j in 0..delChar.length
-#		if (str[i] == delChar[j])
-#			flag = false
-#		else flag = true
-#		end
-#	end
-	
-	flag1 = delChar1.include? str[i]        # true
-	
-	puts flag
-	
-	if (flag == false)
-		newStr.push(str[i])
-	end
-	
-end
-
-#
-#str.delete("Р")
-#str.delete("р")
-#str.delete("Н")
-#str.delete("н")
-#str.delete("К")
-#str.delete("к")
-
-str1 = newStr.join("")
-	puts str1
-
+#puts str.encode('utf-8')
+str = str.gsub /[\u041A\u043A\u041D\u043D\u0420\u0440]/, ""
+puts str
