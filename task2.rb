0fbb8b7615a8c631c7e9ab94e2ba4ee1baeac82a
luba-kn <luba_kn@mail.ru>
2018-05-31 08:40:48 +0300

    v2

--- task2.rb
+++ task2.rb
@@ -9,7 +9,22 @@
 #> 1
 
 # encoding: utf-8
-if ARGV.length < 2
+
+def sravnit(a,b)
+	res = true
+	for i in 0..a.length-1
+		if ( a[i] <=> b[i] ) == 0
+			res = true
+		else 
+			res = false
+			return res
+			exit
+		end
+	end
+	return res
+end
+
+if ( ARGV.length < 2 || ARGV.empty? )
 	puts "Мало данных"
 	exit
 elsif ARGV.length >= 3
@@ -28,45 +43,90 @@ n2 = str2.length
 #str1 = str1.split("")
 #str2 = str2.split("")
 
-if (n1>=1 && n1 <=10000 && n2>=1 && n2<=10000)
+flag = sravnit(str1,str2)
+
+if ((1..10000).include?(n1) && (1..10000).include?(n2))
 	if (n1 != n2)
 		res = -1
 		puts res
 		exit
-	elsif ((str1 <=> str2) == 0)
+	elsif (flag)
 		res = 0
 		puts res
 		exit
-	end	
-		
-		for i in 0..(n2)
-			if ((str1 <=> str2) != 0)
-				if count==n2
-					res=-1
-					puts res
-					exit
-				end
-				
-				puts str2[0]
-				for j in 0..(n2-2)
-										
-					str3[j] = str2[j+1]
-					
-				end	
-				str3[n2-1] = str2[0]
-				str2 = str3
-				puts str2.join("")
-			count+=1
-				
-			else 
-				res = count
-				puts res
-				
-			end
-		
+	end
+end
+	
+	
+for i in 0..(n1)
+	flag = sravnit(str1,str2)
+	if !(flag)
+			
+		if count == n1
+			res = -1
+			puts res
+			exit
 		end
-	
-	
+				
+		lastEl = str1[0]
+		for j in 0..(n2-2)
+										
+			str3[j] = str1[j+1]
+		end	
+
+		str3.delete_at(n2-1)
+		str3.push(lastEl)
+		str1 = str3
+#		puts str1.join("")
+		count+=1
+
+	else 
+		res1 = count
+#		puts res1
+		
+	end
+
 end
 
-puts res
+	str1 = ARGV[0]
+	count = 0
+for i in 0..(n1)
+	flag = sravnit(str1,str2)
+	if !(flag)
+			
+		if count == n1
+			res = -1
+			puts res
+			exit
+		end
+				
+		firstEl = str1[n2-1]
+		str3 = []
+		str3[0] = firstEl
+		for j in 0..(n2-2)
+										
+			str3.push(str1[j])
+		end	
+		
+		str1 = str3
+#		puts str1.join("")
+		count+=1
+
+	else 
+		res2 = count
+#		puts res2
+		
+	end
+
+end
+
+#puts res1
+#puts res2
+
+if (res1 > res2)
+	res = res2
+	puts res
+else
+	res = res1
+	puts res
+end
